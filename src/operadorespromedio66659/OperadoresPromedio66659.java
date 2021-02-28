f3f7bc1a8b07a07a64b1f1db6f109569b8c4964b
Johnny Reveles <Johnny Reveles@LAPTOP-PO4S021M>
2021-02-27 20:54:18 -0600

    Operadores con promedio

---
+++ src/operadorespromedio66659/OperadoresPromedio66659.java
@@ -0,0 +1,57 @@
+/*
+ * UNIVERSIDAD AUTÓNOMA DE CAMPECHE
+ * Facultad de Ingeniería
+ * Ingeniería en Sistemas Computacionales
+ */
+package operadorespromedio66659;
+
+/**
+ *
+ * @author Johnny Reveles
+ */
+public class OperadoresPromedio66659 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+  
+    // VALORES
+ 
+    int valor1 = 10;
+    int valor2 = 9;
+    int valor3 = 8;
+    int valor4 = 7;
+    int valor5 = 6;
+            
+            
+     // PROMEDIO E IMPRIMIR VALORES
+    
+    System.out.println("valor1 = " + valor1);
+    System.out.println("valor2 = " + valor2);
+    System.out.println("valor3 = " + valor3);
+    System.out.println("valor4 = " + valor4);
+    System.out.println("valor5 = " + valor5);
+    System.out.println("promedio = " + (valor1 + valor2 + valor3 + valor4 + valor5) / 5);
+    
+    
+    
+     }
+    
+
+ }
+    
+          
+         
+      
+    
+
+
+
+    
+           
+   
+
+                    
+
